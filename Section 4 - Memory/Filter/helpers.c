b21cda5e61228d931cfeb1d94f1b633a7351dd9e
jf-may <juarezferriolmay@proton.me>
2026-04-21 20:49:52 +0000

    Non elegant solution to the reflect image logic.

--- Section 4 - Memory/Filter/helpers.c
+++ Section 4 - Memory/Filter/helpers.c
@@ -1,10 +1,12 @@
 #include "helpers.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     const double one_third = 0.333333333333333333333333333333333333333333333333;
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -15,12 +17,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = grayscale_pixel;
         }
     }
+
     return;
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE(*temp_image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp_image[i][j] = image[i][j];
+        }
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp_image[i][width - j];
+        }
+    }
+
     return;
 }
 
