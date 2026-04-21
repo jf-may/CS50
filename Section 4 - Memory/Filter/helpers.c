9842bd670f03ba91c042d88b27ece4582e9b9c49
jf-may <juarezferriolmay@proton.me>
2026-04-21 20:15:14 +0000

    Added grayscale filter logic and restructured the gitignore.

--- Section 4 - Memory/Filter/helpers.c
+++ Section 4 - Memory/Filter/helpers.c
@@ -1,8 +1,20 @@
 #include "helpers.h"
+#include "stdio.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    const double one_third = 0.333333333333333333333333333333333333333333333333;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            BYTE grayscale_pixel = (int) (image[i][j].rgbtBlue + image[i][j].rgbtRed + image[i][j].rgbtGreen) * one_third;
+            image[i][j].rgbtBlue = grayscale_pixel;
+            image[i][j].rgbtGreen = grayscale_pixel;
+            image[i][j].rgbtRed = grayscale_pixel;
+        }
+    }
     return;
 }
 
