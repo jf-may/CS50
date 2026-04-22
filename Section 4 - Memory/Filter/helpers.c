97e7a225f72e59cf5b002da80c909e82f682da8c
jf-may <juarezferriolmay@proton.me>
2026-04-22 00:40:36 +0000

    Added blur logic. Potential improvement using padding instead of two loops and boundary check.

--- Section 4 - Memory/Filter/helpers.c
+++ Section 4 - Memory/Filter/helpers.c
@@ -1,5 +1,6 @@
 #include "helpers.h"
 #include "stdio.h"
+#include "stdlib.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -40,8 +41,49 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+// Checks image neighbors. Helper function for box blur
+
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE(*temp)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    int sum_b, sum_g, sum_r, count;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            sum_b = sum_g = sum_r = count = 0;
+
+            for (int di = -1; di <= 1; di++)
+            {
+                for (int dj = -1; dj <= 1; dj++)
+                {
+                    int ind_i = i + di;
+                    int ind_j = j + dj;
+
+                    if (ind_i >= 0 && ind_i < height && ind_j >= 0 && ind_j < width)
+                    {
+                        sum_b += image[ind_i][ind_j].rgbtBlue;
+                        sum_g += image[ind_i][ind_j].rgbtGreen;
+                        sum_r += image[ind_i][ind_j].rgbtRed;
+                        count++;
+                    }
+                }
+            }
+            temp[i][j].rgbtBlue = (int) sum_b / count;
+            temp[i][j].rgbtGreen = (int) sum_g / count;
+            temp[i][j].rgbtRed = (int) sum_r / count;
+        }
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+
     return;
 }
